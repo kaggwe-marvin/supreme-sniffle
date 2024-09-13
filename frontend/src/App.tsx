23100b659e528463e19bffce2a33d65b8f2f6f6a
saybaba <kiwanukamarvin597@gmail.com>
2024-09-13 12:51:57 +0300

    proxy setup in development

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-import { useState } from 'react' 
+import { useState, useEffect } from 'react' 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './components/ui/card'
 
 function App() {
   const [totalSpent, setTotalSpent] = useState(0)
+
+  useEffect(()=>{
+  async function fetchTotal() {
+  const res = await fetch("/api/expenses/total-spent")
+  const data = await res.json()
+  setTotalSpent(data.total)
+  }
+  fetchTotal()
+  }, [])
 
   return (
     <>
